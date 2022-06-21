c56efc64caa9021cad4ec6086ce3303ef2a93e1b
Steven Kwok <derekkwok2020@gmail.com>
2022-06-21 09:16:27 +0100

    SimpleIO
    lecture completed

---
+++ SimpleFileIO/App.java
@@ -0,0 +1,24 @@
+package SimpleFileIO;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+        PrintWriter out = new PrintWriter(new FileWriter("OutFile.txt"));
+        out.println("this is a line in my file...");
+        out.println("a second line in my file...");
+        out.println("a third line in my file...");
+        out.flush();
+        out.close();
+         */
+        Scanner sc = new Scanner(new BufferedReader(new FileReader("OutFile.txt")));
+        // go through the file line by line
+        while(sc.hasNextLine()) {
+            String currentLine = sc.nextLine();
+            System.out.println(currentLine);
+        }
+
+    }
+}
