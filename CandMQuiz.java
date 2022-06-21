a16e8a6224bb7ec59467e4b23edebe96a5366c96
Steven Kwok <derekkwok2020@gmail.com>
2022-06-21 09:00:51 +0100

    Quiz
    completed, not full marks

---
+++ CandMQuiz.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class CandMQuiz {
+    public static void main(String[] args) {
+        /*
+        Set<String> numbers = new HashSet<>();
+        numbers.add("One");
+        numbers.add("Three");
+        numbers.add("Three");
+        numbers.add("Four");
+
+        System.out.println(numbers.size());
+    }
+
+
+
+        //
+        Map<String, String> foodCombos = new HashMap<>();
+        foodCombos.put("PeanutButter", "Jelly");
+        foodCombos.put("Mac", "Cheese");
+        foodCombos.put("Ham", "Cheese");
+        foodCombos.put("Fish", "Chips");
+        foodCombos.put("Chicken", "Waffles");
+        String whatAmI = foodCombos.get("Cheese");
+        System.out.println(whatAmI);
+
+         */
+        List<Boolean> truthsAndMore = new ArrayList<>();
+        truthsAndMore.add(true);
+        truthsAndMore.add(false);
+        truthsAndMore.add(true);
+        truthsAndMore.add(false);
+        truthsAndMore.add(true);
+
+        System.out.println(truthsAndMore.size());
+    }
+}
